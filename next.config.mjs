724c27a75dac567618f4cbc4239a12940728f227
SetFodi <lukafartenadze2004@gmail.com>
2026-01-28 04:43:43 +0400

    updated next, package, tsconfig, globals for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [],
-    },
-    webpack: (config, { dev, isServer }) => {
-      // Temporarily disable CSS minimization to fix build issue
-      if (!dev && !isServer) {
-        config.optimization.minimizer = config.optimization.minimizer.filter(
-          (plugin) => plugin.constructor.name !== 'CssMinimizerPlugin'
-        );
-      }
-      return config;
-    },
-  }
-  
+  images: {
+    domains: [],
+  },
+
+}
+
 export default nextConfig
